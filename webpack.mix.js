8c43dc5f5cafb14ed71aeda700249cfcb465c185
折原七瀬 <n-orihara@cmind-co.jp>
2020-07-09 00:09:52 +0900

    【課題提出】第3章 - Laravel実践 - 第7部 - blade laravelmix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/admin.scss', 'public/css')
+   //課題6 以下追加
    .sass('resources/sass/profile.scss', 'public/css');
