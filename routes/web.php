36b93f7a257a774e46c22cd4d0bb8fc2d19e964e
折原七瀬 <n-orihara@cmind-co.jp>
2020-07-11 12:18:55 +0900

    【課題提出】第3章 - Laravel実践 - 第9部 - ニュース投稿画面

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
     Route::get('profile/create', 'Admin\ProfileController@add');
+    // 課題3
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
+    // 課題6
     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
