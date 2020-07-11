f33107eb2cc15124f77f336832587bb7060e5712
折原七瀬 <n-orihara@cmind-co.jp>
2020-07-11 12:08:55 +0900

    【練習】第3章 - Laravel実践 - 第9部 - ニュース投稿画面

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -50,7 +50,6 @@
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
-                            <!-- 課題1 -->
                             <!-- Authentication Links -->
                             {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                             @guest
